cc48e6c63d69e21a16ba382a1e484e3786811bf8
serkansentilay <sentiilay@gmail.com>
2022-10-19 22:27:49 +0300

    Initial commit

---
+++ todo.js
@@ -0,0 +1,81 @@
+// SELECTORS
+let taskInput = document.querySelector("#task") //input id value
+let liveToastBtn = document.querySelector("#liveToastBtn") //button id value
+let list = document.querySelector("#list") // ul id value
+let addAlert = document.querySelector("#liveToast") // div toast id value
+
+
+
+// EVENTS
+liveToastBtn.addEventListener("click", newList)
+document.addEventListener("DomContentLoaded", displayLocalStorage())
+
+
+
+// FUNCTIONS
+function newList() {
+    if (taskInput.value) {
+        createList(taskInput.value)
+        setLocalStorage(taskInput.value)
+        taskInput.value = ""
+    } else {
+        $(".error").toast("show")
+    }
+}
+
+function createList(todo) {
+    const liDOM = document.createElement("li")
+    liDOM.innerHTML = todo
+    list.appendChild(liDOM)
+
+    const closeBtn = document.createElement("span")
+    closeBtn.classList.add("close")
+    closeBtn.textContent = "\u00D7"
+    liDOM.append(closeBtn)
+
+    closeBtn.onclick = removeList
+    $(".success").toast("show")
+    liDOM.onclick = finishToDo
+}
+
+function removeList() {
+    this.parentElement.remove()
+    deleteLocalStorage(this.previousSibling.textContent)
+}
+
+function finishToDo() {
+    this.classList.toggle("checked")
+}
+
+function getLocalStorage() {
+    let todo;
+    if (localStorage.getItem("todos") === null) {
+        todo = [];
+    } else {
+        todo = JSON.parse(localStorage.getItem("todos"))
+    }
+    return todo;
+}
+
+function displayLocalStorage() {
+    const todos = getLocalStorage()
+    todos.forEach(todo => {
+        createList(todo)
+    });
+}
+
+function setLocalStorage(todo) {
+    let todos = getLocalStorage()
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function deleteLocalStorage(text) {
+    let todos = getLocalStorage()
+    todos.forEach((todo, index) => {
+        if (todo === text) {
+            todos.splice(index, 1)
+        }
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
